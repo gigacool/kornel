2d457279cc98319d68883e00d4714c4f87574433
Cédric HARTLAND <cedric.hartland@gmail.com>
2024-04-11 15:16:02 +0200

    ci: improve build to deliver useable artifact

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,27 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts'
 
-export default {
-  input: 'sources/kornel.ts',
-  output: [
-    {
-      file: 'dist/kornel.js',
-      format: 'esm', 
-      name: 'Kornel',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    typescript(), // Use the TypeScript plugin
-  ],
-};
+export default [
+  {
+    input: 'sources/kornel.ts',
+    output: [
+      {
+        file: 'dist/kornel.js',
+        format: 'esm',
+        name: 'Kornel',
+        sourcemap: true,
+      },
+    ],
+
+
+    plugins: [
+      typescript(), // Use the TypeScript plugin
+    ],
+  },
+  {
+    input: './dist/dts/kornel.d.ts',
+    output: [{ file: 'dist/kornel.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+]
+  ;
