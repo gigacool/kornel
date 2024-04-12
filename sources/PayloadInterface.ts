6667ab238eb9d5210fc193a7ca3e7bc4af0f4dd0
Cédric HARTLAND <cedric.hartland@gmail.com>
2024-04-12 19:31:37 +0200

    fix: create more options for payloads

--- sources/PayloadInterface.ts
+++ sources/PayloadInterface.ts
@@ -1,3 +1,3 @@
 export interface Payload {
-    payload:string
+    payload:any
 }
--- sources/bus/BusInterface.ts
+++ sources/bus/BusInterface.ts
@@ -2,7 +2,7 @@
 
 import { Payload } from "../PayloadInterface";
 
-export type CallbackFunction = (payload:string) => void;
+export type CallbackFunction = (payload:any) => void;
 
 export interface BusInterface {
     listen(channel:string, callback:CallbackFunction):BusInterface;
